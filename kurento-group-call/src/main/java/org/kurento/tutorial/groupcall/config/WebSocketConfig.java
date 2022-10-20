f5de02e072b2ab392e044413eee3a1f560ce8461
yeongjin <hanyj0104@naver.com>
2022-10-20 19:34:01 +0900

    new

---
+++ kurento-group-call/src/main/java/org/kurento/tutorial/groupcall/config/WebSocketConfig.java
@@ -0,0 +1,35 @@
+package org.kurento.tutorial.groupcall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+    //ws로 sokjs connect보냄
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");   // Enables a simple in-memory broker
+
+
+        //   Use this for enabling a Full featured broker likehan RabbitMQ
+
+        /*
+        registry.enableStompBrokerRelay("/topic")
+                .setRelayHost("localhost")
+                .setRelayPort(61613)
+                .setClientLogin("guest")
+                .setClientPasscode("guest");
+        */
+    }
+}
+
